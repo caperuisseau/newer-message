badbfb2f560bebd39ccf1dabe5173c260875b208
caperuisseau <108292873+caperuisseau@users.noreply.github.com>
2024-09-26 20:23:02 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io('https://newer-message.netlify.app', {
+const socket = io('https://newer-message.netlify.app/.netlify/functions/socketHandler', {
     transports: ['websocket'],
 });
 
@@ -6,16 +6,25 @@ const messagesContainer = document.getElementById('messages');
 const messageInput = document.getElementById('messageInput');
 const sendButton = document.getElementById('sendButton');
 
+// Événement pour envoyer un message
 sendButton.addEventListener('click', () => {
-    const message = messageInput.value;
-    if (message.trim() !== '') {
+    const message = messageInput.value.trim();
+    if (message) {
         socket.emit('message', message);
         messageInput.value = ''; // Réinitialiser le champ de saisie
     }
 });
 
+// Événement pour recevoir des messages
 socket.on('message', (message) => {
     const messageElement = document.createElement('div');
     messageElement.textContent = message;
     messagesContainer.appendChild(messageElement);
+});
+
+// Envoi du message quand on appuie sur la touche "Entrée"
+messageInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        sendButton.click();
+    }
 });
